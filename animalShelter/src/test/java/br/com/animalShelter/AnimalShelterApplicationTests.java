c517d7e96020bb668ea23cc8de52208e3e51ce39
Thiago <tthurler10@gmail.com>
2022-06-01 13:14:20 -0300

    Animal-Shelter

---
+++ animalShelter/src/test/java/br/com/animalShelter/AnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.animalShelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
